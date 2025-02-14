6b3692bf6f376d8806ef9ccc425b0fdfcb1d820e
harpreet singh <happybiostockcode07040@gmail.com>
2025-02-14 11:30:13 +0530

    intract and deploy the contract on avnil with the help of titanoboa

---
+++ deploy_favorites_avil.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import boa
+from boa.network import NetworkEnv, EthereumRPC
+from eth_account import Account
+import os
+
+load_dotenv()
+
+ANVIL_URL = os.getenv("ANVIL_URL")
+ANVIL_KEY = os.getenv("ANVIL_KEY")
+
+def main():
+    print("Let's read in the Vyper code and deploy it to the blockchain!")
+
+    my_account = Account.from_key(ANVIL_KEY)
+    env = NetworkEnv(EthereumRPC(ANVIL_URL))
+    boa.set_env(env)
+    boa.env.add_account(my_account, force_eoa=True)
+
+    # Deploy the contract to a `pyevm` network!
+    favorites_contract = boa.load("favorites.vy")
+
+    print("Storing a number...")
+    favorites_contract.store(5)
+
+    print(f"\nFavorite Number: {favorites_contract.retrieve()}\n")
+
+    print("Storing a person...")
+    favorites_contract.add_person("Alice", 25)
+
+    print(f"\nNew Person: {favorites_contract.list_of_people(0)}\n")
+
+
+if __name__ == "__main__":
+    main()
